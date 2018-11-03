f2a5476199099cf3292371fa16e75203c3a48c87
kv1t <kv1t@i.ua>
2018-11-03 23:22:11 +0200

    New changes

--- dz05/dz05_2.py
+++ dz05/dz05_2.py
@@ -7,13 +7,18 @@ def dec(a):
 
 
 def main(func):
-    for i in range(-10, 10, 1):
+    flag = True
+    for i in range(-10, 11, 1):
         if func(i/2) < 0:
-            print(round(float(func(i/2)), 2), end=' ')
+            print(('\x1b[6;30;43m' if flag else '\x1b[6;30;47m') + str(round(float(func(i/2)), 2)) + '\x1b[0m', end=' ')
         else:
-            print('', round(float(func(i/2)), 2), end=' ')
+            print(('\x1b[6;30;43m' if flag else '\x1b[6;30;47m') + ' ' + (str(round(float(func(i/2)), 2)) if i != 0 else '0.0') + '\x1b[0m', end=' ')
+        flag = not flag
 
 
+print('Значение: ', end=' ')
+main(float)
+print('\n','Инкримент: ', sep='', end='')
 main(inc)
-print()
+print('\n','Декримент: ', sep='', end='')
 main(dec)
